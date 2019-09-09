af17832091b447766c599c2349571ee0f8855257
Seiilkan <smyktybekova@gmail.com>
2019-09-08 23:52:52 -0700

    Hello Git!

--- src/Day_01_FirstProgramming/FirstJavaProgram.java
+++ src/Day_01_FirstProgramming/FirstJavaProgram.java
@@ -4,10 +4,9 @@ public class FirstJavaProgram {
    
 	public static void main(String [] args) {
 		
-		System.out. println("Hello Cybertek Batch 12");
-        System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		System.out. println("Hello GitHub");
+       
+		
 }
 
 }
